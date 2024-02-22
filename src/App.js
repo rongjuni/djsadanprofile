3d9dac88298a0879ad1f169a92d4d0ba5d6082b8
[MattSeo] <[rongjuni@hotmail.com]>
2024-02-22 12:21:57 -0500

    initial push

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='main-container'>
+        <div className='top-container'> 여기가 메인 페이지 </div>
+        <p></p>
+        <div className='bottom-container'>
+          <div className='bottom-container-left'>왼쪽거
+            <div> 
+              왼쪽거의 내용물
+            </div>
+          </div>
+          <div className='bottom-container-right'>오른쪽거
+            <div>
+              오른쪽꺼의 내용물
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
